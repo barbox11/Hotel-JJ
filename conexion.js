f545d982a70ec26ac0256afd979b26d22aa8f523
barbox11 <123384500+barbox11@users.noreply.github.com>
2024-07-02 12:02:22 -0500

    conexion establecida con el server falta ingreso de datos

--- conexion.js
+++ conexion.js
@@ -1,17 +1,45 @@
 const mysql = require('mysql');
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+
 
 // Configura la conexión a la base de datos
-const connection = mysql.createConnection({
-    host: '127.0.0.1' ,
-    user: 'root',
-    password: '',
-    database: 'hotel'
+//const connection = mysql.createConnection({
+    //host: '127.0.0.1' ,
+    //user: 'root',
+   // password: '',
+    //database: 'hotel'
+//});
+
+//connection.connect(function(err){
+   // if(err){
+       // throw err;
+   // }else{
+       // console.log("conexon exitosa");
+    //}
+//});
+app.set ("view engine","ejs");
+
+app.get("index.ejs", function(req,res){
+    res.render("index");
 });
 
-connection.connect(function(err){
-    if(err){
-        throw err;
-    }else{
-        console.log("conexon exitosa");
-    }
+app.post("/validar", function(req,res){
+    const datos = req.body;
+
+    console.log(datos);
 })
+
+
+// Inicia el servidor
+app.listen(3000, function(){
+    console.log(`Servidor corriendo en http://127.0.0.1:${3000}`);
+});
+
+
+
+
+
