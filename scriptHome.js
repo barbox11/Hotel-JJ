17d666e6dcdbc018d990b586fbe05560f2dea65b
0Juana0 <juana.torres@unisarc.edu.co>
2024-07-03 16:39:55 -0500

    Diseño listo

--- scriptHome.js
+++ scriptHome.js
@@ -3,6 +3,11 @@ $(document).ready(function() {
     var userRole = "Administrador"; // o "Cliente"
     $('body').attr('data-user-role', userRole);
 
+    var $modal = $("#myModal");
+    var $modalImg = $("#img01");
+    var $captionText = $("#caption");
+    var $close = $(".close");
+
     var tabsVerticalInner = $('#accordian');
     var selectorVerticalInner = $('#accordian').find('li').length;
     var activeItemVerticalInner = tabsVerticalInner.find('.active');
@@ -51,4 +56,48 @@ $(document).ready(function() {
     }
     var target = $('#accordian ul li a[href="' + path + '"]');
     target.parent().addClass('active');
+
+    console.log("Modal:", $modal.length);
+    console.log("Modal Image:", $modalImg.length);
+    console.log("Caption:", $captionText.length);
+    console.log("Close button:", $close.length);
+
+    // Usar delegación de eventos para las imágenes
+    $(document).on('click', '.myImg', function(e) {
+        e.preventDefault(); // Prevenir cualquier comportamiento por defecto
+        console.log("Imagen clickeada:", this.src);
+        $modal.addClass('show');
+        $modalImg.attr("src", this.src);
+        $captionText.html(this.alt);
+    });
+
+    $close.click(function() {
+        $modal.removeClass('show');
+    });
+
+    $(window).click(function(event) {
+        if (event.target == $modal[0]) {
+            $modal.removeClass('show');
+        }
+    });
+
+    // Manejar clics en los enlaces de navegación
+    $('.nav-link').click(function(e) {
+        e.preventDefault(); // Prevenir la navegación por defecto
+        var target = $(this).data('target');
+
+        // Ocultar todas las secciones
+        $('.content-section').removeClass('active');
+
+        // Mostrar la sección objetivo
+        $(target).addClass('active');
+    });
 });
+
+
+
+
+
+
+
+    
